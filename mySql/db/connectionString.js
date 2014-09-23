410baff950f591a0d4527248fb6e737df9cdd253
Lars-Erik Roald <lroal@hpUbuntuLars.(none)>
2014-09-23 21:14:07 +0200

    skeleton for mySql

--- mySql/db/connectionString.js
+++ mySql/db/connectionString.js
@@ -1,1 +1,1 @@
-module.exports = 'postgres://postgres:postgres@localhost/test';
+module.exports = 'mysql://root@localhost/rdbDemo?multipleStatements=true';
--- mySql/db/resetDemo.js
+++ mySql/db/resetDemo.js
@@ -1,25 +1,28 @@
 var fs = require('fs');
 var Promise = require('promise');
 var conString = require('./connectionString');
-var pg = require('pg.js');
+var mySql = require('mysql');
 
 //var drop = "DROP TABLE IF EXISTS _compositeOrderLine;DROP TABLE IF EXISTS _compositeOrder;DROP TABLE IF EXISTS _deliveryAddress;DROP TABLE IF EXISTS _orderLine;DROP TABLE IF EXISTS _order;DROP TABLE IF EXISTS _customer;"
-var drop = 'drop schema public cascade;create schema public;'
-var createCustomer = "CREATE TABLE _customer (cId uuid PRIMARY KEY, cName varchar(40), cBalance numeric, cRegdate timestamp with time zone, cIsActive boolean, cPicture bytea);"
+var drop = 'drop table if exists _customer;' 
+var createCustomer = "CREATE TABLE _customer (cId varchar(36) PRIMARY KEY, cName varchar(40));"
+// var createCustomer = "CREATE TABLE _customer (cId varchar(36) PRIMARY KEY, cName varchar(40), cBalance numeric, cRegdate timestamp with time zone, cIsActive boolean, cPicture bytea);"
 var createOrder = "CREATE TABLE _order (oId uuid PRIMARY KEY, oOrderNo varchar(20), oCustomerId uuid  REFERENCES _customer);"
 var createOrderLine = "CREATE TABLE _orderLine (lId uuid PRIMARY KEY, lOrderId uuid REFERENCES _order, lProduct varchar(40));"
 var createCompositeOrder = "CREATE TABLE _compositeOrder (oCompanyId numeric, oOrderNo numeric, oCustomerId uuid  REFERENCES _customer, PRIMARY KEY (oCompanyId,oOrderNo));";
 var createCompositeOrderLine = "CREATE TABLE _compositeOrderLine (lCompanyId numeric, lOrderNo numeric, lLineNo numeric, lProduct varchar(40), PRIMARY KEY (lCompanyId,lOrderNo, lLineNo));";
 var createDeliveryAddress = "CREATE TABLE _deliveryAddress (dId uuid PRIMARY KEY, dOrderId uuid REFERENCES _order, dName varchar(100), dStreet varchar(200), dPostalCode varchar(50), dPostalPlace varchar(200), dCountryCode varchar(2), dCountry varchar(100));";
 
-var createSql = drop + createCustomer + createOrder + createOrderLine + createDeliveryAddress + createCompositeOrder +  createCompositeOrderLine;
+var createSql = drop + createCustomer;
+// var createSql = drop + createCustomer + createOrder + createOrderLine + createDeliveryAddress + createCompositeOrder +  createCompositeOrderLine;
 var buffer;
 var buffer2;
 
 createBuffers();
 
-var insertCustomer1 = "INSERT INTO _customer VALUES ('a0000000-0000-0000-0000-000000000000','George',177,'2003-04-12 04:05:06 z',false," + buffer +  ");",
-    insertCustomer2 = "INSERT INTO _customer VALUES ('b0000000-0000-0000-0000-000000000000','John',3045,'2014-05-11 06:49:40.297-0200',true," + buffer2 +  ");";
+var insertCustomer1 = "INSERT INTO _customer VALUES ('a0000000-0000-0000-0000-000000000000','George'" +  ");";
+// var insertCustomer1 = "INSERT INTO _customer VALUES ('a0000000-0000-0000-0000-000000000000','George',177,?'2003-04-12 04:05:06 z',false," + buffer +  ");",
+var    insertCustomer2 = "INSERT INTO _customer VALUES ('b0000000-0000-0000-0000-000000000000','John',3045,'2014-05-11 06:49:40.297-0200',true," + buffer2 +  ");";
     insertCustomer3 = "INSERT INTO _customer VALUES ('12345678-0000-0000-0000-000000000000','Yoko',8765,'2012-02-10 07:00:40.297-0200',false," + buffer2 +  ");";
     insertCustomer4 = "INSERT INTO _customer VALUES ('87654321-0000-0000-0000-000000000000','Johnny',8123,'2011-03-11 06:00:40.297-0200',true," + buffer2 +  ");";
     insertCustomer5 = "INSERT INTO _customer VALUES ('87654399-0000-0000-0000-000000000000','Paul',8125,'2011-04-11 06:00:40.297-0200',true," + buffer2 +  ");";
@@ -42,7 +45,8 @@ var insertOrderLines =
     "INSERT INTO _compositeOrderLine VALUES (1,1001,2,'Guide to the galaxy');";
 var insertDeliveryAddress = "INSERT INTO _deliveryAddress values ('dddddddd-0000-0000-0000-000000000000','b0000000-b000-0000-0000-000000000000', 'Lars-Erik Roald', 'Node Street 1', '7030', 'Trondheim', 'NO', 'Norway');"
     
-var insertSql = insertCustomers + insertOrders + insertOrderLines + insertDeliveryAddress;
+// var insertSql = insertCustomers + insertOrders + insertOrderLines + insertDeliveryAddress;
+var insertSql = insertCustomer1;
 
 function createBuffers() {
     buffer = newBuffer([1, 2, 3]);
@@ -55,13 +59,14 @@ function createBuffers() {
 }
 
 function insert(onSuccess, onFailed) {
-    var client = new pg.Client(conString);
+    var client = mySql.createConnection(conString);
     client.connect(function(err) {
         if (err) {
             console.log('Error while connecting: ' + err);
             onFailed(err);
             return;
         }
+        console.log(createSql + insertSql);
         client.query(createSql + insertSql, onInserted);
 
         function onInserted(err, result) {
